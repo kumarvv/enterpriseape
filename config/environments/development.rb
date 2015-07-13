afecddd0abadb3099beaad6cdf9a664f051ad083
Vijay Vijayaram <kumarvv@gmail.com>
2015-07-13 22:53:45 +0000

    Login/Logout/Register links updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   # devise - mailer 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://vjrails-111809.nitrousapp.com:3000' }
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true 
   
