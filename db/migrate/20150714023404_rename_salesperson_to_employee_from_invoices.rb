4af0e7ef6916ae823f00623fed97d7f48fe5a359
Vijay Vijayaram <kumarvv@gmail.com>
2015-07-14 02:47:34 +0000

    db based select added

---
+++ db/migrate/20150714023404_rename_salesperson_to_employee_from_invoices.rb
@@ -0,0 +1,6 @@
+class RenameSalespersonToEmployeeFromInvoices < ActiveRecord::Migration
+  def change
+    remove_column :invoices, :salesperson_id, :integer
+    add_column :invoices, :employee_id, :integer
+  end
+end
